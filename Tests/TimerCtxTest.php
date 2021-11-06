17baf0b10d86c4a1ae815a906b999d944f18425f
zhao <1060656096@qq.com>
2021-11-06 11:50:40 +0800

    cca: 增加测试

--- Tests/TimerCtxTest.php
+++ Tests/TimerCtxTest.php
@@ -38,7 +38,7 @@ class TimerCtxTest extends TestCase
     /**
      * 只取消一个 child context
      */
-    public function testWithCancelOnlyOneChildrenCancel() {
+    public function testWithTimoutOnlyOneChildrenCancel() {
         $ctx0 = Background();
         /* @var TimerCtx $ctx1 */
         list($ctx1, $cancelFunc1) = WithTimeout($ctx0, Time::Second * 1);
@@ -61,4 +61,82 @@ class TimerCtxTest extends TestCase
         $this->assertTrue($ctx31->Done(), '$ctx31->Done()');
 
     }
+
+
+    /**
+     * 只取消 child context
+     */
+    public function testWithTimoutOneChildrenCancel1() {
+        $ctx0 = Background();
+        /* @var TimerCtx $ctx1 */
+        list($ctx1, $cancelFunc1) = WithTimeout($ctx0, Time::Second * 1);
+        $ctx2Value = WithValue($ctx1, "ctx2Value", "ctx2Value.data");
+        /* @var CancelCtx $ctx3 */
+        list($ctx3, $cancelFunc3)  = WithCancel($ctx2Value);
+        /* @var CancelCtx $ctx31 */
+        list($ctx31, $cancelFunc31)  = WithCancel($ctx3);
+        /* @var TimerCtx $ctx1 */
+        list($ctx4, $cancelFunc4) = WithTimeout($ctx31, Time::Second * 1);
+
+        $ctx5Value = WithValue($ctx4, "ctx2Value", "ctx2Value.data");
+        /* @var CancelCtx $ctx6 */
+        list($ctx6, $cancelFunc6)  = WithCancel($ctx5Value);
+        /* @var CancelCtx $ctx7 */
+        list($ctx7, $cancelFunc7)  = WithCancel($ctx6);
+
+        $this->assertFalse($ctx1->Done(), '$ctx1->Done()');
+        $this->assertFalse($ctx2Value->Done(), '$ctx2Value->Done()');
+        $this->assertFalse($ctx3->Done(), '$ctx3->Done()');
+        $this->assertFalse($ctx31->Done(), '$ctx31->Done()');
+
+        $cancelFunc1();
+        $this->assertTrue($ctx1->Done(), '$ctx1->Done()');
+        $this->assertFalse($ctx2Value->Done(), '$ctx2Value->Done()');
+        $this->assertTrue($ctx3->Done(), '$ctx3->Done()');
+        $this->assertTrue($ctx31->Done(), '$ctx31->Done()');
+        $this->assertTrue($ctx4->Done(), '$ctx4->Done()');
+        $this->assertFalse($ctx5Value->Done(), '$ctx5Value->Done()');
+        $this->assertTrue($ctx6->Done(), '$ctx6->Done()');
+        $this->assertTrue($ctx7->Done(), '$ctx7->Done()');
+
+    }
+
+    /**
+     * 只取消 child context
+     */
+    public function testWithTimoutChildrenCancel2() {
+        $ctx0 = Background();
+        /* @var TimerCtx $ctx1 */
+        list($ctx1, $cancelFunc1) = WithTimeout($ctx0, Time::Second * 1);
+        $ctx2Value = WithValue($ctx1, "ctx2Value", "ctx2Value.data");
+        /* @var CancelCtx $ctx3 */
+        list($ctx3, $cancelFunc3)  = WithCancel($ctx2Value);
+        /* @var CancelCtx $ctx31 */
+        list($ctx31, $cancelFunc31)  = WithCancel($ctx3);
+        /* @var TimerCtx $ctx1 */
+        list($ctx4, $cancelFunc4) = WithTimeout($ctx31, Time::Second * 1);
+
+        $ctx5Value = WithValue($ctx4, "ctx2Value", "ctx2Value.data");
+        /* @var CancelCtx $ctx6 */
+        list($ctx6, $cancelFunc6)  = WithCancel($ctx5Value);
+        /* @var CancelCtx $ctx7 */
+        list($ctx7, $cancelFunc7)  = WithCancel($ctx6);
+
+        $this->assertFalse($ctx1->Done(), '$ctx1->Done()');
+        $this->assertFalse($ctx2Value->Done(), '$ctx2Value->Done()');
+        $this->assertFalse($ctx3->Done(), '$ctx3->Done()');
+        $this->assertFalse($ctx31->Done(), '$ctx31->Done()');
+
+        $cancelFunc4();
+        $this->assertFalse($ctx1->Done(), '$ctx1->Done()');
+        $this->assertFalse($ctx2Value->Done(), '$ctx2Value->Done()');
+        $this->assertFalse($ctx3->Done(), '$ctx3->Done()');
+        $this->assertFalse($ctx31->Done(), '$ctx31->Done()');
+        $this->assertTrue($ctx4->Done(), '$ctx4->Done()');
+        $this->assertFalse($ctx5Value->Done(), '$ctx5Value->Done()');
+        $this->assertTrue($ctx6->Done(), '$ctx6->Done()');
+        $this->assertTrue($ctx7->Done(), '$ctx7->Done()');
+
+    }
+
 }
